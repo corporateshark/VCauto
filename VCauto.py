017b0a796e9c0d6430f9293e27d4dccfa608e880
Sergey Kosarevsky <sk@linderdaum.com>
2017-11-07 03:48:00 +0300

    Fixed Python 2 compatibility

--- VCauto.py
+++ VCauto.py
@@ -35,6 +35,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import print_function
 import os
 import sys
 import uuid
